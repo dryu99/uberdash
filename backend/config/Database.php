efa68e9baba76015694c89bfecde2ed3771ca7bf
Daniel Ryu <rujjiryu99@gmail.com>
2020-06-08 21:13:43 -0700

    add more documentation to Database class

--- backend/config/Database.php
+++ backend/config/Database.php
@@ -34,6 +34,19 @@
       OCILogoff($this->db_conn);
     }
 
+    /**
+     * Run a SQL statement
+     *
+     * Call like:
+     *     db->execute("INSERT INTO MyTable VALUES (:c1, :c2)",
+     *                  array(array(":c1", $c1),
+     *                        array(":c2", $c2)))
+     *
+     * Note: this performs a commit if the sql is valid and doesn't violate any constraints.
+     *
+     * @param string $sql The statement to run
+     * @param array $bindvars Binds. An array of (bv_name, php_variable)
+     */
     public function execute($sql, $bindvars = array()) {
       $this->statement = OCIParse($this->db_conn, $sql);
 
@@ -42,9 +55,16 @@
           OCIBindByName($this->statement, $bv[0], $bv[1]);
       }
 
-      OCIExecute($this->statement); // will auto commit
-  }
+      OCIExecute($this->statement);
+    }
 
+    /**
+     * Run a query and return all rows.
+     *
+     * @param string $sql A query to run and return all rows
+     * @param array $bindvars Binds. An array of (bv_name, php_variable)
+     * @return array An array of rows
+     */
     public function executeFetchAll($sql, $bindvars = array()) {
       $this->execute($sql, $bindvars);
       oci_fetch_all($this->statement, $result, null, null, OCI_FETCHSTATEMENT_BY_ROW);
