ddf980b56c9ff50f3db48be641730ac79f30a106
Daniel Ryu <rujjiryu99@gmail.com>
2020-06-16 14:26:09 -0700

    implement REST endpoint for group by aggregation operation

---
+++ backend/api/order_information/getGroupCountsForDeliverer.php
@@ -0,0 +1,51 @@
+<?php 
+  // Imports
+  include_once '../../config/Database.php';
+  
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+
+  // init DB & connect
+  $database = new Database();
+  $database->connect();  
+
+  // get params from request
+  $deliv_phone_number = isset($_GET['DelivererPhoneNumber']) 
+    ? $_GET['DelivererPhoneNumber'] 
+    : die(http_response_code(404));
+
+  $group_type = isset($_GET['Group']) 
+    ? $_GET['Group'] 
+    : die(http_response_code(404));
+
+  // have to specify table alias for Restaurant attributes
+  if ($group_type === 'RESTAURANT_NAME' || $group_type === 'RESTAURANT_ADDRESS') {
+    $group_type = "r.$group_type";
+  }
+
+  // create and execute query
+  $query = "SELECT $group_type, COUNT(*) AS COUNT
+    FROM OrderInformation oi
+    INNER JOIN RestaurantLocation rl
+      ON oi.Restaurant_Address = rl.Restaurant_Address
+    INNER JOIN Restaurant r
+      ON rl.Restaurant_Name = r.Restaurant_Name
+    INNER JOIN Customers c
+      ON oi.Customer_PhoneNumber = c.Customer_PhoneNumber
+    INNER JOIN OrderStatus os
+      ON oi.OrderStatus_ID = os.OrderStatus_ID
+    WHERE oi.Deliverer_PhoneNumber = :DelivererPhoneNumber
+    GROUP BY $group_type";
+  $bindvars = [[":DelivererPhoneNumber", $deliv_phone_number]];               
+  $result = $database->executeFetchAll($query, $bindvars);   
+
+  // init response variable containing query result
+  $response = $result;
+
+  // return response in JSON format
+  echo json_encode($response, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE|JSON_NUMERIC_CHECK);
+
+  // disconnect from DB
+  $database->disconnect();
+?>
